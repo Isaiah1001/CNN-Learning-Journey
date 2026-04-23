9fdc5cfeb25ec34b1cc1e37d327e9c897d3d83df
Hui Z <93089536+Isaiah1001@users.noreply.github.com>
2026-04-23 19:55:00 +0800

    Delete 03_experiment_management/lighting_module directory

--- 03_experiment_management/lighting_module/model/count_parameters.py
+++
@@ -1,6 +0,0 @@
-#model/count_parameters.py
-def count_parameters(model):
-    """count the number of parameters in the model"""
-    total_params = sum(p.numel() for p in model.parameters())
-    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    return total_params, trainable_params
--- 03_experiment_management/lighting_module/postprocess/__init__.py
+++
@@ -1,3 +0,0 @@
-from .plot_fig import plot_image, plot_image_LD, plot_training_metrics
-
-__all__ = ["plot_image", "plot_image_LD", "plot_training_metrics"]
